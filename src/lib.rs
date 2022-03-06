e99d8d9f05327f65f114919a0831b74bd1902bec
newton-raphson <newton@mokshya.io>
2022-03-06 21:11:34 +0545

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+//solana program to perform cpi
 use solana_program::{
   account_info::{AccountInfo,next_account_info},
   entrypoint,
   entrypoint::ProgramResult,
-  pubkey::Pubkey,
+  pubkey::{Pubkey, PUBKEY_BYTES},
   program_error::ProgramError,
   msg,
   instruction::AccountMeta,
@@ -12,26 +13,26 @@ use solana_program::{
 entrypoint!(cpi_use);
 
  pub fn cpi_use(
-    program_id: &Pubkey, 
+    _program_id: &Pubkey, 
     account_info: &[AccountInfo], 
     instruction_data: &[u8],
 )-> ProgramResult
 {
     let account_info_iter = &mut account_info.iter();
-    let (number, rest) = instruction_data.split_at(8);
+    let (number, rest) = instruction_data.split_at(8); //number : number of accounts required to operate an instruction
     let number = number.try_into().map(u64::from_le_bytes).or(Err(ProgramError::MissingRequiredSignature))?;
+    let (program, rest) = unpack_pubkey(rest)?;
     let mut metas: Vec<AccountMeta> = Vec::with_capacity(std::mem::size_of::<AccountMeta>()*(number as usize));
-    let mut i=0;
+    let mut i=0; 
     while i < number
     {
       let account_used = next_account_info(account_info_iter)?; 
       let tmp_meta: AccountMeta=AccountMeta::new(*account_used.key, account_used.is_signer);
       metas.push(tmp_meta);
       i=i+1;
-
     }
 
-    let instruction=instruction::Instruction::new_with_bytes(*program_id, rest,metas);
+    let instruction=instruction::Instruction::new_with_bytes(program, rest,metas);
     //cpi is 
       invoke(
       &instruction, 
@@ -40,4 +41,11 @@ entrypoint!(cpi_use);
         
     Ok(())
 
+}
+pub(crate) fn unpack_pubkey(input: &[u8]) -> Result<(Pubkey, &[u8]), ProgramError> {
+  let pk = input
+      .get(..PUBKEY_BYTES)
+      .map(Pubkey::new)
+      .ok_or(ProgramError::MissingRequiredSignature)?;
+  Ok((pk, &input[PUBKEY_BYTES..]))
 }
